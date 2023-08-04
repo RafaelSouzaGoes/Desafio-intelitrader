293081193a7ec58441c9149f9536afe0e8a19d83
Rafael Goes <lanchaster@gmail.com>
2023-08-04 16:26:49 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio_intelitrader/Desafio_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_01
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao Aplicativo Console");
+        }
+
+    }
+}
